7cc56b4b1f0855889c28ae680973b2052fd92920
Manthan Bagade <178804616+cryptic-mortal@users.noreply.github.com>
2026-04-04 12:12:32 +0530

    test(mem_generator): add backward compatibility with the previous processor

---
+++ mem_generator/code_negative.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 4;
+    int b = 5;
+    int c = a-b;
+    return c;
+}
---
+++ mem_generator/xor.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int x = 40;
+    int y = 23;
+    int z = x^y;
+    return z;
+}
